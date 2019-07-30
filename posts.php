2fe9217fbd364d8425959256683f45e9714bf681
Tijana Miscevic <stamparijaboje@gmail.com>
2019-07-30 20:37:11 +0200

    Zadatak 2 - done

--- posts.php
+++ posts.php
@@ -1,34 +1,4 @@
-
-<!doctype html>
-<html lang="en">
-<head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
-
-    <title>Vivify Blog</title>
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-
-    <!-- Custom styles for this template -->
-    <link href="styles/blog.css" rel="stylesheet">
-</head>
-
-<body>
-
-<?php include "header.php" ?>
-
-<main role="main" class="container">
-
-    <div class="row">
-
-        <div class="col-sm-8 blog-main">
-
-            <div class="blog-post">
+<div class="blog-post">
                 <h2 class="blog-post-title">Sample blog post</h2>
                 <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
 
@@ -91,16 +61,3 @@
                 <a class="btn btn-outline-primary" href="#">Older</a>
                 <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
             </nav>
-
-        </div><!-- /.blog-main -->
-
-        <?php include "sidebar.php" ?>
-
-    </div><!-- /.row -->
-
-</main><!-- /.container -->
-
-<?php include "footer.php" ?>
-
-</body>
-</html>
